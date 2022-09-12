5b53c0bdfd560c4cf0bdee08f900fd8e2e215c2f
dreamnaphat <dreamnaphat1997@gmail.com>
2022-09-12 20:48:43 +0700

    update comment route.

--- routes/chapter.js
+++ routes/chapter.js
@@ -2,18 +2,23 @@ const express = require('express')
 const router = express.Router()
 const ChapterController = require('../controllers/ChapterController')
 
+/* get all chapter */
 router.get('/', async (request, response) => {
     response.send(await ChapterController.index())
 })
+/* get 1 chapter */
 router.get('/:id', async (request, response) => {
     // response.send(await Chapter.show(request))
 })
+/* create chapter */
 router.post('/', async (request, response) => {
     response.status(201).send(await ChapterController.create(request))
 })
+/* update chapter */
 router.put('/:id', async (request, response) => {
     // response.send(await Chapter.update(request))
 })
+/* delete chapter */
 router.delete('/:id', async (request, response) => {
     // response.send(await Chapter.destroy(request))
 })
